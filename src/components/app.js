28ec3d5dbc9ef87d9ffc6ba7f6d5a166cc1e5eb0
alexisspa9 <alexisspa9@gmail.com>
2020-10-07 09:40:57 +0300

    complete

---
+++ src/components/app.js
@@ -0,0 +1,16 @@
+// src/components/app.jsx
+
+// Import dependencies
+import React from 'react'
+
+// Create main App component
+const App = () => (
+  <div>
+    <h1>Welcome to Alpa Desktop</h1>
+
+    <p>Let's start building your awesome desktop app with electron and React!</p>
+  </div>
+)
+
+// Export the App component
+export default App
